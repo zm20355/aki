4a20c5a21a8645937281e1df1d9c295de090a1e0
zm20355 <131091759+zm20355@users.noreply.github.com>
2023-10-17 10:27:11 +0900

    Update ex6-5.js

--- ex6-5.js
+++ ex6-5.js
@@ -2,5 +2,5 @@ var who65='world';
 function sayhello65(){alert('Hello,' + who65 + '!');}
 function taro65(){who65='太郎';}
 function hanako65(){who65='花子';}
-function taro66(){'が来た';}
-function hanako66(){'が来た';}
+function taro66(){alert('who65' + 'が来た';}
+function hanako66(){alert('who65' + 'が来た';}
