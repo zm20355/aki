ad9061679a89bd2b332b98a7e2c3a2cbc7ae6f33
zm20355 <131091759+zm20355@users.noreply.github.com>
2023-10-24 10:29:24 +0900

    Update ex6-88.js

--- ex6-88.js
+++ ex6-88.js
@@ -1,1 +1,1 @@
-fuction gazo688(){document.getElementById('who688').innerHTML='<img src="./image/2.png" alt="オン">';}
+function gazo688(){document.getElementById('who688').innerHTML='<img src="./image/2.png" alt="オン">';}
