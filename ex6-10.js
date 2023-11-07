5884b8284cbd41eabd811adc0cfc03f8149bbaf1
zm20355 <131091759+zm20355@users.noreply.github.com>
2023-11-07 10:02:09 +0900

    Update ex6-10.js

--- ex6-10.js
+++ ex6-10.js
@@ -1,2 +1,2 @@
-function imgchange1(){document.getElementById('logo').src='./image/2.png';}
-function imgchange2(){document.getElementById('logo').src='./image/1.png';}
+function imgchange1(){document.getElementById('suuzi').src='./image/2.png';}
+function imgchange2(){document.getElementById('suuzi').src='./image/1.png';}
