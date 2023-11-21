bbb9fa87897eb0ab43a2318ea94f63fcec942c7d
zm20355 <131091759+zm20355@users.noreply.github.com>
2023-11-21 10:17:12 +0900

    Update ex6-13.js

--- ex6-13.js
+++ ex6-13.js
@@ -1,4 +1,4 @@
 function mes(){alert("３秒経ちました！");}
 function hyoji13(){var str13=document.timer13.moji13.value.length;
-                   if(cnt13 < 11 ){document.timer13.moji.value=str13.substr(0,cnt13+1);}else{document.timer13.moji13.value="";}
+                   if(cnt13<11){document.timer13.moji.value=str13.substr(0,cnt13+1);}else{document.timer13.moji13.value="";}
                    setTimeout("hyoji13()",1000);}
