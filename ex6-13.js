6451d6ea516c9d1e9a8e6f02257eddf25976ebb9
zm20355 <131091759+zm20355@users.noreply.github.com>
2023-11-21 09:49:59 +0900

    Create ex6-13.js

---
+++ ex6-13.js
@@ -0,0 +1,3 @@
+function hyoji13(){car str13=document.timer13.moji13.value.length;
+                   if(cnt13 < 11 ){document.timer13.moji.value=str13.substr(0,cnt13+1);}else{document.timer13.moji13.value="";}
+                   setTimeout('hyoji13()',1000);}
