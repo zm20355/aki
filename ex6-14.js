8676e32343ac4e2a4545977a256a2cdf0fb99f7f
zm20355 <131091759+zm20355@users.noreply.github.com>
2023-11-28 10:01:09 +0900

    Update ex6-14.js

--- ex6-14.js
+++ ex6-14.js
@@ -1,2 +1,2 @@
-function sanchange1(){document.getElementById("itini")."src=./image/2.png";}
-function sanchange2(){document.getElementById("itini")."src=./image/1.png";}
+function sanchange1(){document.getElementById('itini').'src=./image/2.png';}
+function sanchange2(){document.getElementById('itini').'src=./image/1.png';}
