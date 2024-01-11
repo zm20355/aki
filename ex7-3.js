bf052b67c66a003a9ce2413cfa1966b69316fd12
zm20355 <131091759+zm20355@users.noreply.github.com>
2024-01-11 11:41:34 +0900

    Create ex7-3.js

---
+++ ex7-3.js
@@ -0,0 +1,6 @@
+function BMI() {
+    var cm = Number(document.getElementById('cm').value) / 100; 
+    var kg = Number(document.getElementById('kg').value);
+    var bmi = kg / (cm * cm);
+ alert('あなたのBMIは ' + bmi + ' です。');
+}
