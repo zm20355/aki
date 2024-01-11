f8e19d578268c4097cb90664604458aa40bb83e3
zm20355 <131091759+zm20355@users.noreply.github.com>
2024-01-11 12:12:44 +0900

    Update ex6-13.js

--- ex6-13.js
+++ ex6-13.js
@@ -1,5 +1,7 @@
 function mes(){alert("３秒経ちました！");}
-function hyoji13(){var str13="一文字ずつ表示します。";
-                   var cnt13=document.timer13.moji13.value.length;
-                   if(cnt13<11){document.timer13.moji13.value=str13.substr(0,cnt13+1);}else{document.timer13.moji13.value="";}
-                   setTimeout("hyoji13()",1000);}
+function hyoji13(){
+  var str13="一文字ずつ表示します。";
+  var cnt13=document.timer13.moji13.value.length;
+  if(cnt13<11){document.timer13.moji13.value=str13.substr(0,cnt13+1);}else{document.timer13.moji13.value="";}
+ setTimeout("hyoji13()",1000);
+}
