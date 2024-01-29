8cdbd83dde44b614f704d96ce0a1fdad87524601
zm20355 <131091759+zm20355@users.noreply.github.com>
2024-01-29 15:51:33 +0900

    Update ex7-11.js

--- ex7-11.js
+++ ex7-11.js
@@ -1,10 +1,8 @@
 function determineLeapYear(){
  var year = Number(document.getElementById('year').value);
- var answer711;  
- if ((year % 4 == 0 && nenn % 100 != 0 )|| year % 400 == 0)
- {
-   answer711='閏年';}else{answer711 = '平年';
- }  
- document.getElementById('answer7112').innerText = '西暦' + year + '年は' + answer711;
- alert('西暦' + year + '年は' + answer711);
+ if ((X % 4 == 0 && X % 100 != 0) || (X % 400 == 0)) 
+ {alert('西暦' + year + '年は閏年');}
+ else
+ {alert('西暦' + year + '年は平年');}
 }
+
